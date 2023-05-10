c2fd77f7f00f36edea1a2ccac173cb83fb744e03
jsnxx <130407774+jsnxx@users.noreply.github.com>
2023-05-09 23:15:28 -0400

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -13,7 +13,7 @@ THANK YOU AND HAVE A NICE SUMMER!
 
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+import plotly as px
 import pydeck as pdk
 #The package plotly is a package that we did not use in class.
 
